34e0e6e8e0c174ffec594352a77020a893b5984d
Erensu Deveci <erensudeveci@gmail.com>
2025-05-19 10:41:36 +0300

    Seed data added and migrated to database for Villa entity.

--- WhiteLagoon.Infrastructure/Migrations/20250519074035_mig_seed_data_for_villa.Designer.cs
+++ WhiteLagoon.Infrastructure/Migrations/20250519074035_mig_seed_data_for_villa.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WhiteLagoon.Infrastructure.Data;
 namespace WhiteLagoon.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250519074035_mig_seed_data_for_villa")]
+    partial class mig_seed_data_for_villa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
