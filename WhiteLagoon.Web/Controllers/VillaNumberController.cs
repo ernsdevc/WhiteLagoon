f930e2bcea7d44753f1798eabb466880af5c05c8
Erensu Deveci <erensudeveci@gmail.com>
2025-05-20 11:16:29 +0300

    Create method added for VillaNumber entity to VillaNumberController in Web project.

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -16,6 +17,24 @@ namespace WhiteLagoon.Web.Controllers
         {
             var villaNumbers = _db.VillaNumbers.ToList();
             return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber villaNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(villaNumber);
+                _db.SaveChanges();
+                TempData["success"] = "The villa number created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
