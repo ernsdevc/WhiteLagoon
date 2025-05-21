2baf4fedff64b4ce62150d7a6e6b199d6b58c3b7
Erensu Deveci <erensudeveci@gmail.com>
2025-05-21 20:53:56 +0300

    Save method removed from VillaRepository and added to UnitOfWork.

--- WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
+++ WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
@@ -18,5 +18,10 @@ namespace WhiteLagoon.Infrastructure.Repository
             _db = db;
             Villa = new VillaRepository(_db);
         }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
