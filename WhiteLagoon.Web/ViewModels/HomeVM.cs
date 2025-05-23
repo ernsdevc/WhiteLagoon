48894c3eb58f4f872bd0b87d4b091289b99965ac
Erensu Deveci <erensudeveci@gmail.com>
2025-05-23 22:27:08 +0300

    Index view of Home and layout, HomeController edited.

---
+++ WhiteLagoon.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly ChecInDate { get; set; }
+        public DateOnly ChecOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
